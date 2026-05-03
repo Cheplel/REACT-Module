231085596cb4fcd6794f367ba39d15ce0bb5cbd6
Cheplel <jean.koech@student.moringaschool.com>
2026-05-04 01:48:10 +0300

    Add files via upload

---
+++ items.jsx
@@ -0,0 +1,8 @@
+export const projectlist = [ 
+{id : 1, title: 'Social site', done: true, option: 'Assignment'},
+{id : 2, title: 'To Do List', done: true, option: 'Practice'},
+{id : 3, title: 'Book Store', done: false, option: 'Assignment'},
+{id : 4, title: 'Shopping List', done: true, option: 'Practice'}
+]
+
+export const options = ["Practice", "Assignment"];
